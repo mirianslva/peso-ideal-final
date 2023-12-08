a7ff12eaa525e84c40dbcc875a8aa604a1d80816
mirianslva <142011303+mirianslva@users.noreply.github.com>
2023-12-08 14:10:36 +0000

    commit

--- App.js
+++ App.js
@@ -1,75 +1,60 @@
-import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, Text, View,Switch, TextInput, Image, ImageBackground } from 'react-native';
-import styles from './globalStyles';
-
-// http://conversorfacil.com.br/calcular/peso-ideal
-import imagem1 from './assets/miles.jpg'
-import imagem2 from './assets/drew.jpg'
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   const [altura, setAltura] = useState('');
-  const [sexo, setSexo] = useState(false);
-  const [peso,setPeso] = useState('');
-  const [imagem, setImagem] = useState('');
+  const [peso, setPeso] = useState('');
 
-  const calcular = (altura,sexo,peso,imagem) => {
-    if(sexo == false){
-      let h = parseInt(altura)
-      setPeso((( h - 100 ) - [ ( h - 150 ) / 4 ]) + "Kg")
-    } else if(sexo == true){
-      let h = parseInt(altura)
-      setPeso((( h - 100 ) - [ ( h - 150 ) / 2 ])+ "Kg" )
-    };
-    // if(sexo==false){
-    //   setImagem(imagem1)
-    // } else if(sexo == true){
-    //   setImagem(imagem2)
-    // };
+  const calcH = (altura) => {
+    let inth = parseFloat(altura);
+    setPeso((inth - 100) - [(inth - 150) / 4]);
   };
+
+  const calcM = (altura) => {
+    let inth = parseFloat(altura);
+    setPeso((inth - 100) - [(inth - 150) / 2]);
+  };
+
+
+
   return (
     <View style={styles.container}>
-      <ImageBackground source={!sexo ? imagem1 : imagem2} resizeMode="cover" style={{width: '100%', height: "100%"}}>
-      <View style={styles.content}>
-        <View style={styles.vidro}>
-        <Text style={styles.title}>Peso ideal</Text>
-        <Text style={styles.textoSimples}>Altura</Text>
-        <View style={styles.viewInput}>
+      <Text>Peso Ideal</Text>
+      <Text>Altura em cm</Text>
+      <View style={styles.viewInput}>
           <TextInput
-          value={altura}
-          onChangeText={setAltura}
-          style={styles.input}
-          keyboardType='numeric'
+            value={altura}
+            onChangeText={setAltura}          
+            keyboardType="number-pad"
+            placeholder="Digite a altura"
+            style={styles.input}
           />
-        </View>
-        <Text style={styles.textoSimples}>Sexo</Text>
-        <View style={{display: "flex", flexDirection: "row",  alignItems: "center", justifyContent: "center"}}>
-          <Text style={styles.homem}>Homem</Text>
-          <Switch
-                trackColor={{ false: '#b1deef', true: '#ffcbdb' }}
-                thumbColor={sexo ? '#ff69b4' : '#298DC7'}
-                ios_backgroundColor="#298DC7"
-                onValueChange={setSexo}
-                value={sexo}
-          />
-          <Text style={styles.mulher}>Mulher</Text>
-        </View>
-        <View style={{display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingTop:15,}}>
-          <Pressable
-            style={!sexo ? styles.botao : styles.botao2}
-            onPress={() => calcular(altura,sexo,peso,imagem)}
-          >
-            <Text style={{color: "#fff", fontWeight: "bold"}}>
-              Calcular
-            </Text>
-          </Pressable>
-
-          <View style={{alignItems:"center", justifyContent: "center"}}><Text style={{color: "#fff", fontWeight:"bold"}}>{`${peso}`}</Text></View>
-        </View>
-        <StatusBar style="auto" />
-        </View>
       </View>
-      </ImageBackground>
+      <Text>Sexo</Text>
+      <Pressable
+            onPress={() => calcM(altura)}
+            style={styles.botao}>
+            <Text style={{ fontSize: 15, color: 'black' }}>
+              calcular peso ideal
+            </Text>
+      </Pressable>
+      <View style={styles.center}>
+          <View style={styles.botaoResultado}>
+            <Text style={styles.textoBotao}>Área Resultante: </Text>
+            <Text style={styles.textoBotao}>{peso}</Text>
+          </View>
+        </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- globalStyles.js
+++ globalStyles.js
@@ -39,7 +39,6 @@ export default styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    // justifyContent: 'center',
 
   },
   vidro:{
